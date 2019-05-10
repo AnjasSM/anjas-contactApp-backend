63d73e3419b7e07d1f59ea030dd971ea8f48d003
AnjasSM <syarifudin.anjas@gmail.com>
2019-05-11 06:16:12 +0700

    add backend

---
+++ component/update.js
@@ -0,0 +1,13 @@
+const db    = require('./db');
+const args  = process.argv.slice(2);
+const query = `UPDATE contacts
+               SET name   ='${args[1]}',
+                   phone  ='${args[2]}',
+                   email  ='${args[3]}',
+                   gender ='${args[4]}'
+               WHERE id=${args[0]}`;
+
+db.run(query, function (err) {
+  if (err) throw err;
+  console.log('contact updated!');
+});
